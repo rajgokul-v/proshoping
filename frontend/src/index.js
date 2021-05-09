1fa99b65ff45ceed50113b00d92272b37c6230ef
Raj Gokul <rajgokul.v2000@gmail.com>
2021-05-09 22:22:01 +0530

    store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './components/bootstrap.min.css'
-
+import { Provider } from 'react-redux'
 import App from './components/App'
+import store from './components/store'
 
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root')
 )
