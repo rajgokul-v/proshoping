144715636757d297159ff546b6dfcce64cd61db5
Raj Gokul V <rajgokul.v2000@gmail.com>
2022-01-14 13:33:22 +0530

    User authentication endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs'
 import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema(
@@ -25,7 +26,9 @@ const userSchema = mongoose.Schema(
 		timestamps: true
 	}
 )
-
+userSchema.methods.matchPassword = async function (enteredpassword) {
+	return await bcrypt.compare(enteredpassword, this.password)
+}
 const User = mongoose.model('User', userSchema)
 
 export default User
