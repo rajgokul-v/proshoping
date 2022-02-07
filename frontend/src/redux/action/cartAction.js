c7b63e8c2fb992bbad8ee25da93d852bc611fadb
gokul raj <rajgokul.v2000@gmail.com>
2022-02-07 17:13:12 +0530

    message 4

--- frontend/src/redux/action/cartAction.js
+++ frontend/src/redux/action/cartAction.js
@@ -21,3 +21,5 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
 	dispatch({ type: CART_REMOVE_ITEM, payload: id })
 	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
+
+asdfadsfsdfsda
